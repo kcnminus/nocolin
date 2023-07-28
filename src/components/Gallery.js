462951c29b610a28e3f327235df9bba15cd3bd39
Colin No <colin.no@policygenius.com>
2023-07-27 23:37:21 -0400

    should pull up modal on click and render exif data now

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,6 +5,7 @@ import EXIF from 'exif-js';
 const Gallery = () => {
   const [imageData, setImageData] = useState([]);
   const [exifData, setExifData] = useState([]);
+  const [enlargedImage, setEnlargedImage] = useState(null);
 
   // useEffect will confirm that my component has mounted and then run fetchImageUrls
   useEffect(() => {
@@ -13,7 +14,17 @@ const Gallery = () => {
         // Fetch image URLs and update state
         const imageFiles = require.context('../assets', false, /\.(jpe?g)$/);
         const urls = imageFiles.keys().map(imageFiles);
+
+        const imageBlobs = await Promise.all(urls.map(async (url) =>{
+          const response = await fetch(url);
+          const blob = await response.blob();
+          return blob;
+        }));
+
+        const exifDataArray = await Promise.all(imageBlobs.map(blob => getExif(blob)));
+
         setImageData(urls);
+        setExifData(exifDataArray);
       } catch (error) {
         console.error('Error fetching images:', error);
       }
@@ -25,6 +36,7 @@ const Gallery = () => {
     return new Promise((resolve) => {
       EXIF.getData(imageBlob, function () {
         const exifData = EXIF.getAllTags(this);
+        console.log('Exif Data:', exifData);
         resolve(exifData)
       });
     });
@@ -34,15 +46,29 @@ const Gallery = () => {
     <div className="max-w-screen-xl mx-auto">
       <h2>Gallery</h2>
       <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12">
-        {imageData.map((imageUrl, index) => (
-          <img
-          key={index}
-          src={imageUrl}
-          alt={`by Colin ${index + 1}`}
-          className="w-full h-full object-scale-down"
-          />
-        ))}
+        {imageData.map((imageUrl, index) => {
+          const { Make, Model, ShutterSpeedValue, ApertureValue, FocalLength, ISO } = exifData[index] ?? {};
+          return(
+          <div className="relative w-full h-full" key={index}>
+            {console.log('State Exif Data:', exifData)}
+            <img
+              src={imageUrl}
+              alt={`by Colin ${index + 1}`}
+              className="w-full h-full object-scale-down cursor-pointer"
+              onClick={() => setEnlargedImage(imageUrl)}
+            />
+            <div className="absolute bottom-0 bg-white bg-opacity-50 w-full text-center">
+              {Make} {Model} {FocalLength} {ApertureValue} {ShutterSpeedValue} {ISO}
+            </div>
+          </div>
+          );
+        })};
       </div>
+      {enlargedImage && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-10">
+          <img src={enlargedImage} className="max-h-full max-w-full object-contain cursor-pointer" onClick={() => setEnlargedImage(null)} />
+        </div>
+      )}
     </div>
   );
 };
