ae5d13a6818ad8f909d8fdb76d153a81c36686ef
Colin No <colin.no@policygenius.com>
2023-07-31 22:08:22 -0400

    small change to profile

--- src/components/Background.js
+++ src/components/Background.js
@@ -18,7 +18,7 @@ const Background = () => {
             <div className="flex flex-wrap justify-center">
               <div className="w-full lg:w-9/12 px-4">
                 <p className="mb-20 text-lg leading-relaxed text-gray-800">
-                   Though I didn't know it at the time, I first started learning to code in the heyday of MySpace and Xanga, when it was possible to customize one's homepage using HTML and CSS. I went to a pretty well-known STEM-focused high school in New York City, but had no idea it was possible to code for money until just a few years ago! 
+                   Though I didn't know it at the time, I first started learning to code in the heyday of MySpace and Xanga, when it was possible to customize one's homepage using HTML and CSS. I went to a pretty well-known STEM-focused high school in New York City, but had no idea it was possible to make a career out of writing code until just a few years ago! 
 
                    I enjoy learning, and have been making an effort to push new code every day. I am also working on certifications in frontend, backend, and AWS Cloud. 
                 </p>
