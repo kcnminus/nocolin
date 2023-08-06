7ae8c2fb1c3544f33aecd97912970ebefa3655cb
Colin No <colin.no@policygenius.com>
2023-08-06 17:04:59 -0400

    last bits of style, pt.2

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,7 +7,7 @@ import { faFacebookSquare, faLinkedin , faInstagram, faGithub } from '@fortaweso
 const Bio = () => {
   return (
     <div className="bg-gray-300 container mx-auto px-4">
-      <div className="relative flex flex-col min-w-0 break-words bg-gray-300 w-full mb-6 shadow-xl rounded-lg mt-16 md:mt-32">
+      <div className="relative flex flex-col min-w-0 break-words bg-gray-300 w-full md:mb-6 shadow-xl rounded-lg mt-2 md:mt-32">
         <div className="px-6">
           <div className="flex flex-wrap justify-center">
             <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
@@ -15,13 +15,13 @@ const Bio = () => {
                 <img
                   alt="..."
                   src={profilePic}
-                  className="shadow-xl rounded-full h-auto align-middle border-none relative flex m-16 ml-20 lg:ml-16"
+                  className="shadow-xl rounded-full h-auto align-center border-none relative flex m-8 md:m-16 ml-10 lg:ml-16"
                   style={{ maxWidth: "300px" }}
                 />
               </div>
             </div>
-          <div className="text-center mt-12">
-            <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
+          <div className="text-center md:mt-12">
+            <h3 className="text-4xl font-semibold leading-normal text-gray-800 mb-2">
               Colin No
             </h3>
             <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
@@ -33,10 +33,10 @@ const Bio = () => {
               Software Engineer
             </div>
           </div>
-          <div className="mt-10 py-10 border-t border-gray-300 text-center">
+          <div className="mt-2 md:mt-10 py-5 md:py-10 border-t border-gray-300 text-center">
             <div className="flex flex-wrap justify-center">
               <div className="w-full lg:w-9/12 px-4">
-                <p className="mb-4 text-lg leading-relaxed text-gray-800">
+                <p className="mb-2 md:mb-4 text-lg leading-relaxed text-gray-800">
                   Hey! I'm Colin. I'm a software engineer specializing in JavaScript and Python.
                   This website consists of small samples of my work in software engineering, music, and photography. 
                   Feel free to check out my Facebook, LinkedIn, Instagram, and Github using the respective icons for each. 
@@ -44,7 +44,7 @@ const Bio = () => {
               </div>
             </div>
           </div>
-          <div className="leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none">
+          <div className="leading-none px-3 py-4 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none">
             <ul className="flex flex-row items-center list-none ml-auto">
               <li className="nav-item">
                 <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="https://www.facebook.com/ colincno">
